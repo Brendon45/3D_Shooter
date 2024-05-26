a4aff6797af2b02ab58a07c82103e553160e9493
Tafara <c.tafaranyamhunga@gmail.com>
2024-05-26 21:36:09 +0000

    3d Maze game.

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Pointer to string arguments passed to the program
+ *
+ * Return: On success (0),
+ *			else failure status if failed.
+ */
+
+int main(int argc, char **argv)
+{
+
+	return (0);
+}
+
