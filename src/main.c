4ea2b9ff85b1f277d88e2314d4d97e282c05ae48
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-29 13:02:11 +0200

    Drawing a window.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <SDL2/SDL.h>
 
 /**
  * main - Entry point
@@ -9,9 +9,27 @@
  *			else failure status if failed.
  */
 
-int main(int argc, char **argv)
+int main(void)
 {
+	/* Drawing the window */
+	SDL_Window *window = NULL;
+	SDL_Renderer *renderer = NULL;
+
+	/* Initialize SDL, width and height */
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer(640 * 4, 480 * 4, 0, &window, &renderer);
+	SDL_RenderSetScale(renderer, 4, 4);
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	/* Drawing on the window and centering it */
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderDrawPoint(renderer, 640/2, 480/2);
+
+	/* Make sure the window doesn't close immediately */
+	SDL_RenderPresent(renderer);
+	SDL_Delay(10000);
 
 	return (0);
 }
-
