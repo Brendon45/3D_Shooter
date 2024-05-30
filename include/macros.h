168a4e51c7d6de0b9980941727829a83e1d49ddc
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-30 22:26:40 +0200

    Game macros.

---
+++ include/macros.h
@@ -0,0 +1,11 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+/* Predefined (global) variables */
+#define SCREEN_HEIGHT 768
+#define SCREEN_WIDTH 1024
+#define MAP_WIDTH 24
+#define MAP_HEIGHT 24
+
+#endif  /* MACROS_H */
+
