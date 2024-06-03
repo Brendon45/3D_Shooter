2b9d68272f65b06ec6a97b582b9695305439bae3
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-03 11:17:14 +0200

    Creating the maze.

---
+++ include/create_maze.h
@@ -0,0 +1,10 @@
+#ifndef CREATE_MAZE_H
+#define CREATE_MAZE_H
+
+char **create_map(char *, double_s *, int_s *, size_t *);
+void plot_grid_points(char **, double_s *, int_s *, size_t, size_t, char *,
+		      int *);
+size_t get_line_count(char *);
+size_t get_char_count(char *);
+
+#endif  /* CREATE_MAZE_H */
