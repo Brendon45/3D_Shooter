595380c26f4ec4a3adb0e44426880fe72499d795
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-03 11:24:13 +0200

    Predefined variables.

--- include/macros.h
+++ include/macros.h
@@ -14,11 +14,11 @@
 #define FALSE 0
 
 /* Colors */
-#define REND_COLOR_WHITE(x) SDL_SetRenderDrawColor(x, 255, 255, 255, 255)
-#define REND_COLOR_BLACK(x) SDL_SetRenderDrawColor(x, 0, 0, 0, 255)
-#define REND_COLOR_YELLOW(x) SDL_SetRenderDrawColor(x, 255, 255, 0, 255)
-#define REND_COLOR_GREEN(x) SDL_SetRenderDrawColor(x, 0, 255, 0, 255)
-#define REND_COLOR_BLUE(x) SDL_SetRenderDrawColor(x, 0, 0, 255, 255)
-#define REND_COLOR(x, r, g, b, a) SDL_SetRenderDrawColor(x, r, g, b, a)
+#define RENDER_COLOR_WHITE(x) SDL_SetRenderDrawColor(x, 255, 255, 255, 255)
+#define RENDER_COLOR_BLACK(x) SDL_SetRenderDrawColor(x, 0, 0, 0, 255)
+#define RENDER_COLOR_YELLOW(x) SDL_SetRenderDrawColor(x, 255, 255, 0, 255)
+#define RENDER_COLOR_GREEN(x) SDL_SetRenderDrawColor(x, 0, 255, 0, 255)
+#define RENDER_COLOR_BLUE(x) SDL_SetRenderDrawColor(x, 0, 0, 255, 255)
+#define RENDER_COLOR(x, r, g, b, a) SDL_SetRenderDrawColor(x, r, g, b, a)
 
 #endif  /* MACROS_H */
