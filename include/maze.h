9cedb5fdfe1372817c42aae233ea1aeecc563e6d
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-03 11:24:42 +0200

    Game entry point.

--- include/maze.h
+++ include/maze.h
@@ -12,23 +12,27 @@
 #include <ctype.h>
 
 /* Include headers */
+#include "create_maze.h"
+#include "draw.h"
+#include "events.h"
+#include "macros.h"
+#include "memory.h"
+#include "structures.h"
 
-
-/* init_instance.c */
 int init_instance(SDL_Instance *);
 
-/* world_creation.c */
+
 level *build_world_from_args(int, char **);
 
-/* movement.c */
+
 void rotate(double_s *, double_s *, int);
 void movement(keys, double_s *, double_s *, double_s *, char **);
 
-/* func_win.c */
+
 void print_win(void);
 int check_win(double_s, int_s, int *);
 
-/* dist_checks.c */
+
 double get_wall_dist(char **, double_s *, int_s *, int_s *, double_s *, int *,
 		     double_s *, double_s *);
 void check_ray_dir(int_s *, double_s *, double_s, int_s, double_s, double_s);
